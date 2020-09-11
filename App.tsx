5bb797eba9367d57b77235b7c1c3da39c5c9f6ac
Gabriel Pajak <pajak.gabriel@gmail.com>
2020-09-11 21:06:28 +0200

    Better navigation.

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Icon } from 'react-native-elements'
 import Footer from './screens/Footer';
 
 function HomeScreen({navigation}) {
@@ -27,7 +28,7 @@ function PlacesList({navigation}) {
 
 function PlaceHomeScreen({navigation}) {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator tabBar={props => <MyTabBar {...props} />}>
       <Tab.Screen name="Places" component={PlacesList} />
       <Tab.Screen name="Add" component={AddPlace} />
     </Tab.Navigator>
@@ -38,6 +39,56 @@ function AddPlace({navigation}) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Add</Text>
+    </View>
+  );
+}
+
+function MyTabBar({ state, descriptors, navigation }) {
+  return (
+    <View style={{ flexDirection: 'row' }}>
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+            ? options.title
+            : route.name;
+
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: 'tabPress',
+            target: route.key,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name);
+          }
+        };
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: 'tabLongPress',
+            target: route.key,
+          });
+        };
+
+        return (
+          <TouchableOpacity
+            accessibilityRole="button"
+            accessibilityLabel={options.tabBarAccessibilityLabel}
+            testID={options.tabBarTestID}
+            onPress={onPress}
+            onLongPress={onLongPress}
+            style={{ flex: 1 }}
+          >
+            {/* <Icon name='rowing'></Icon> */}
+            <Icon name={route.name === 'Add' ? 'add' : 'list'} type='material'></Icon>
+          </TouchableOpacity>
+        );
+      })}
     </View>
   );
 }
